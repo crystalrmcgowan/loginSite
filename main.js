bed8f5c36c027c1b14ab6b60cc963db342485b15
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-30 11:11:27 -0400

    added body-parser

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const app = express()
 const mustacheExpress = require("mustache-express")
-const session = require("express-session")
+const expressSession = require("express-session")
+const bodyParser = require("body-parser")
 
 app.engine("mst", mustacheExpress())
 app.set("views", "./views")
 app.set("view engine", "mst")
 
 app.use(express.static("public"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   res.render("home")
